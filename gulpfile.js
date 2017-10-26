9b97ecaca1ca8d311e7e7cc9d05e1bb1b44169fa
Caree Claunch <caree@caree.codes>
2017-10-26 14:39:32 -0500

    added live-reload to gulp file...I think?

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,12 @@
 var gulp = require('gulp');
 
 var liveReload = require('live-reload');
+
+gulp.task('default', function(){
+    console.log('sup');
+});
+
+
+gulp.task('live-reload', function(){
+    console.log('live-reload');
+})
